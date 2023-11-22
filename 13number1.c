312e53d988a1c745b77156f5c302d888cf5f35fe
Tousart <150588467+Tousart@users.noreply.github.com>
2023-11-22 15:09:47 +0300

    Update 13number1.c

--- 13number1.c
+++ 13number1.c
@@ -3,18 +3,9 @@
 #include <time.h>
 #define n 5
 
-int main()
+int *sortik(int *arr)
 {
-    int *arr, i, j, min, ind;
-    srand(time(NULL));
-    arr = (int*)malloc(n*sizeof(int));
-    printf("Массив:\n");
-    for (i = 0; i < n; i++)
-    {
-        arr[i] = rand() % 10;
-        printf("%d ", arr[i]);
-    }
-    printf("\nОтсортированный массив (выбором):\n");
+    int i, j, min, ind;
     for (i = 0; i < n; i++)
     {
         min = 999;
@@ -28,7 +19,25 @@ int main()
         }
         arr[ind] = arr[i];
         arr[i] = min;
+    }
+    return arr;
+}
+
+int main()
+{
+    int *arr, i;
+    srand(time(NULL));
+    arr = (int*)malloc(n*sizeof(int));
+    printf("Массив:\n");
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10;
         printf("%d ", arr[i]);
+    }
+    printf("\nОтсортированный массив (выбором):\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", sortik(arr)[i]);
     }
     return 0;
 }
