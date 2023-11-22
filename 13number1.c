34113f145db94f0e3ff17e565d9d3a115154f26f
Tousart <150588467+Tousart@users.noreply.github.com>
2023-11-22 14:16:01 +0300

    Create 13number1.c

---
+++ 13number1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define n 5
+
+int main()
+{
+    int *arr, i, j, min, ind;
+    srand(time(NULL));
+    arr = (int*)malloc(n*sizeof(int));
+    printf("Массив:\n");
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10;
+        printf("%d ", arr[i]);
+    }
+    printf("\nОтсортированный массив (выбором):\n");
+    for (i = 0; i < n; i++)
+    {
+        min = 999;
+        for (j = i; j < n; j++)
+        {
+            if (arr[j] < min)
+            {
+                min = arr[j];
+                ind = j;
+            }
+        }
+        arr[ind] = arr[i];
+        arr[i] = min;
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
