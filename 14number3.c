1b44f73684ac1c9c1451a72aec5a64b80fb2a2bd
Tousart <150588467+Tousart@users.noreply.github.com>
2023-11-29 15:47:26 +0300

    Update 14number3.c

--- 14number3.c
+++ 14number3.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctupe.h>
+#include <ctype.h>
 
-void change(char *s, char elem, int *count)
+void change(char *s, char elem, int count, int *a)
 {
     for (int i = 0; i < strlen(s); i++)
     {
-        if (s[i] == elem) s[i] = toupper(s[i]);
+        if (s[i] == elem) 
+        {
+        s[i] = toupper(s[i]);
+        count += 1;
+        }
         printf("%c", s[i]);
     }
-    
+    *a = count;
 }
 
 int main()
 {
     char elem, *s;
-    void (*pointer)(char *s, char elem, int *count);
-    int count = 0, i;
+    void (*pointer)(char *s, char elem, int count, int *a);
+    int count = 0, i, a = 0;
     s = (char*)calloc(5, sizeof(char));
     printf("Введите строку: ");
     gets(s);
     printf("\nВведите элемент: ");
     scanf("%c", &elem);
-    printf("\n");
     pointer = change;
-    pointer(s, elem, &count);
+    pointer(s, elem, count, &a);
+    printf("\nКоличество замен: %d", a);
     return 0;
 }
